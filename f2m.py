9a7b88fd412772f4e24b2377615f69f2632909b6
Ega Fosso <egafosso@gmail.com>
2021-04-23 00:21:36 +0700

    Main window set up

---
+++ f2m.py
@@ -0,0 +1,9 @@
+from tkinter import Tk, Button, Label, DoubleVar, Entry
+
+window = Tk()
+window.title("Conversion: pied en metre")
+window.configure(background="light gray")
+window.geometry("320x220")
+window.resizable(width=False, height=False)
+
+window.mainloop()
